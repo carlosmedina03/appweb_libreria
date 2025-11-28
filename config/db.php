75e591a23ff1189e454556bf1868d4f5b39ce43d
LuisGuillermo-RamosDiaz <contact@guillermoramos.dev>
2025-11-28 10:14:02 -0700

    feat/estructura-bd

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+// Responsable: Rol 4 (Back-End)
+// Descripción: Conexión global a base de datos usando MySQLi.
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db   = 'libreria_db';
+// $mysqli = new mysqli(...);
+?>
